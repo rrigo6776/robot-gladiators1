eb3b2c4843d5020cd38f8cf02620c2d463720885
Rigo Ram <rigoram6776@gmail.com>
2023-04-13 08:48:17 -0700

    random numbers

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,7 @@ var fight = function(enemyName) {
    
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney -  10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
     }
@@ -33,7 +33,8 @@ var fight = function(enemyName) {
 
     if (promptFight === "fight" || promptFight === "FIGHT") {
 
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack)
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health reamining."
@@ -48,9 +49,8 @@ var fight = function(enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    //
-
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3 , enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
 
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -81,7 +81,9 @@ var startGame = function(){
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1 ));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            //the parameter is set so when the function is called it will substitute these two numbers as min and max values
+            //since the function is being hoisted so it re-assigns the value here
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
 
             if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -176,5 +178,9 @@ var shop = function () {
     }
 }
 
-startGame();
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min); 
+    return value; 
+}
 
+startGame();
